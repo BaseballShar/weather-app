c8b6616c4ad02b230f33495561ec9426c8bc2b2b
Baseball Shar <workingcth@gmail.com>
2024-06-05 22:53:30 +0800

    Fixed UV description display

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -4,7 +4,7 @@ export default function WeatherCard({ weatherData }) {
   const humidity = weatherData.humidity.data[0].value;
   const rainfall = weatherData.rainfall.data[7].max;
   const uvValue = weatherData.uvindex === "" ? 0 : weatherData.uvindex.data[0].value;
-  const uvDesc = weatherData.uvindex === "" ? 0 : weatherData.uvindex.data[0].desc;
+  const uvDesc = weatherData.uvindex === "" ? "" : (weatherData.uvindex.data[0].desc);
 
   return (
     <div className="weather-card">
@@ -12,7 +12,7 @@ export default function WeatherCard({ weatherData }) {
         <p>Temperature: {temperature}°C</p>
         <p>Humidity: {humidity}%</p>
         <p>Rainfall: {rainfall}mm</p>
-        <p>UV: {uvValue} ({uvDesc})</p>
+        <p>UV: {uvValue} {uvDesc}</p>
       </div>
       <div>
         <img className="w-20" src={weatherIconUrl} alt="weather icon" />
