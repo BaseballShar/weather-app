040f1b7f17b7da90bb4a96c8efa00fe538b07be1
Baseball Shar <workingcth@gmail.com>
2024-06-05 22:40:20 +0800

    Added sun emoji

--- components/SunCard.jsx
+++ components/SunCard.jsx
@@ -2,11 +2,27 @@ export default function SunCard({data}) {
   const rise = data.data[0][1]
   const transit = data.data[0][2]
   const set = data.data[0][3]
+
+  function getSunIcon(rise, set) {
+    const today = new Date()
+    const minute = today.getMinutes()
+    const hour = today.getHours()
+    const time = `${hour}:${minute}`
+
+    /* Render day time emoji */
+    if (time <= set && time >= rise) {
+      return 127774
+      /* Render night time emoji */
+    } else {
+      return 11088
+    }
+  }
+
   return (
     <div className="weather-card">
-      <p>
-        Picture of sun / season
-      </p>
+      <span className="text-8xl">
+        {String.fromCodePoint(getSunIcon(rise, set))}
+      </span>
       <div>
         <p>Sun rise: {rise}</p>
         <p>Sun transit: {transit}</p>
