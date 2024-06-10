2cc390546ac75c17212f90c7ee57264aef9cdc16
Baseball Shar <workingcth@gmail.com>
2024-06-10 15:23:27 +0800

    Added header navigation and footer information

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { FaGithub, FaHome, FaLinkedin } from "react-icons/fa";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -8,10 +8,42 @@ export const metadata = {
   description: "Powered by HKO",
 };
 
+const space = " ";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="flex justify-between items-center bg-blue-200 px-8 py-2">
+          <a href="https://baseballshar.github.io">
+            <FaHome className="header-icon" />
+          </a>
+          <p className="font-mono text-xl">Baseball's Weather panel</p>
+          <div className="flex gap-4">
+            <a href="https://github.com/BaseballShar/weather-app">
+              <FaGithub className="header-icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/chan-tsz-ho-baseball-98b00923b/">
+              <FaLinkedin className="header-icon" />
+            </a>
+          </div>
+        </header>
+        {children}
+        <footer className="flex justify-around bg-blue-200 py-2 text-lg font-mono">
+          <p>
+            Email:{space}
+            <a className="footer-link" href="mailto:workingcth@gmail.com">
+              workingcth@gmail.com
+            </a>
+          </p>
+          <p>
+            Github:{space}
+            <a className="footer-link" href="https://github.com/BaseballShar/">
+              BaseballShar
+            </a>
+          </p>
+        </footer>
+      </body>
     </html>
   );
 }
