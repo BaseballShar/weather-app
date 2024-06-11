fc93d658d7a63bce858a1e5b8d2843d24b62ca02
Baseball Shar <workingcth@gmail.com>
2024-06-11 17:47:53 +0800

    Used media query for header placement

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,45 +1,28 @@
-"use client";
 import { FaGithub, FaHome, FaLinkedin } from "react-icons/fa";
 
 export default function Header() {
-  /* For small devices */
-  if (window.innerWidth < 640) {
-    return (
-      <header className="bg-blue-200 px-8 py-2 gap-4">
-        <p className="font-mono text-xl text-center">
+  return (
+    <header className="bg-blue-200 px-8 py-2 gap-4">
+      {/* Render title as a separate line on medium or smaller devices */}
+      <p className="font-mono text-xl text-center md:hidden">
+        Baseball&#39;s Weather panel
+      </p>
+      <div className="flex justify-between">
+        <a href="https://baseballshar.github.io">
+          <FaHome className="header-icon" />
+        </a>
+        {/* Render title inline on large or larger devices */}
+        <p className="font-mono text-xl hidden md:block">
           Baseball&#39;s Weather panel
         </p>
-        <div className="flex justify-between">
-          <a href="https://baseballshar.github.io">
-            <FaHome className="header-icon" />
+        <div className="flex gap-4">
+          <a href="https://github.com/BaseballShar/weather-app">
+            <FaGithub className="header-icon" />
           </a>
-          <div className="flex gap-4">
-            <a href="https://github.com/BaseballShar/weather-app">
-              <FaGithub className="header-icon" />
-            </a>
-            <a href="https://www.linkedin.com/in/chan-tsz-ho-baseball-98b00923b/">
-              <FaLinkedin className="header-icon" />
-            </a>
-          </div>
+          <a href="https://www.linkedin.com/in/chan-tsz-ho-baseball-98b00923b/">
+            <FaLinkedin className="header-icon" />
+          </a>
         </div>
-      </header>
-    );
-  }
-
-  /* For medium or larger devices */
-  return (
-    <header className="flex justify-between items-center bg-blue-200 px-8 py-2 gap-4">
-      <a href="https://baseballshar.github.io">
-        <FaHome className="header-icon" />
-      </a>
-      <p className="font-mono text-xl">Baseball&#39;s Weather panel</p>
-      <div className="flex gap-4">
-        <a href="https://github.com/BaseballShar/weather-app">
-          <FaGithub className="header-icon" />
-        </a>
-        <a href="https://www.linkedin.com/in/chan-tsz-ho-baseball-98b00923b/">
-          <FaLinkedin className="header-icon" />
-        </a>
       </div>
     </header>
   );
